fbca4d1047d3e886baf6a7c24ea51bab8cf8eaba
Andy1iang <andyliang254@gmail.com>
2023-07-20 13:34:28 -0400

    Added functionality to update missing data to certain language files
    python bundler-for-translation.py --update to initiate update function to update default languages (configured on line 30)

--- bundler-for-translation.py
+++ bundler-for-translation.py
@@ -1,5 +1,6 @@
 '''
 This Python Script should be in the directory of SpiraTeam/Design/Localization
+Need to install beautifulsoup4 before running: pip install beautifulsoup4
 '''
 #importing Regex (Part of Standard Libraries) to check file names
 import re
@@ -11,8 +12,11 @@ import os
 #importing sys (Part of Standard Libraries) to take in command line calls
 import sys
 
-#importing bs4 (Not Part of Standard Libraries) to read & write resx files
+#importing bs4 (Not Part of Standard Libraries) to read resx files
 from bs4 import BeautifulSoup   #To Install on command line:  pip install beautifulsoup4
+
+#importing ElementTree to write to resx files
+import xml.etree.ElementTree as ET
 
 #Script is located in SpiraTeam/Design/Localization
 #Goes up 2 directories to access the SpiraTeam directory
@@ -23,7 +27,7 @@ dir_path = os.path.abspath(os.path.join(sub_path, os.pardir)) #SpiraTeam Directo
 #Default Configuration Class
 class default_config:
     def __init__(self): 
-        self.languagesUpdate = ('es', 'pt') #Edit to change languages to update
+        self.languages_update = ('es', 'pt') #Edit to change languages to update
         self.language = ('fr', 'de') #Edit to Change Default Languages
         self.all_languages = ['cs', 'de', 'es', 'fi', 'fr', 'hu', 'pl', 'pt-pt', 'pt', 'ru', 'zh-Hans', 'zh-Hant'] #Edit to Change Supported Languages
         self.directories = { # Edit to Change Default Directories that Store resx Files
@@ -114,6 +118,26 @@ def import_language(language):
     else: 
         #if zip file is not found, this prints
         print(f'Language file not found, Spira-{language}.zip missing')
+
+#helper function to copy primary and language files to script directory
+def bundle_language(language):
+    '''
+    language: language selected by the user
+    '''
+    #Checking a File with name of Spira-'language' exists, deletes if it is
+    if os.path.exists(f'{dir_path}/Design/Localization/Spira-{language}'):
+        shutil.rmtree(f'{dir_path}/Design/Localization/Spira-{language}')
+
+    #Creates new temporary folder to hold resx files
+    os.mkdir(f'{dir_path}/Design/Localization/Spira-{language}')
+    
+    #Loops through all key-value pairs for default file paths
+    for prefix, directory in ds.directories.items():
+        for files in os.listdir(f'{dir_path}/{directory}'):
+            #saves files to to a temporary folder in Design/Localization
+            copy_files(language,directory,prefix,files)
+            
+    
             
 #function that allows the using to choose a language
 def choose_language():
@@ -133,6 +157,53 @@ def choose_language():
             user_language = user_language.split(',')
         
     return user_language
+
+#helper function to update missing data to language files
+def make_dictionary(language,file_name):
+    '''
+    language: user's choice of language
+    file_name: file name to be read from (resx)
+    '''
+    with open(f'{script_path}/Spira-{language}/{file_name}','r') as f:
+        data = f.read() #reading the file
+        
+        #making beautifulsoup data into a list
+        bs_data = BeautifulSoup(data, "xml")
+        list_data = list(bs_data.findAll('data'))
+        
+        #creating key value pairs for each data element 
+        data_dict = {}
+        for element in list_data:
+            element  = str(element)
+            if re.search(r'name="\w+"',element):
+                name = re.search(r'name="\w+"',element).span()   
+            data_dict[element[name[0]+6:name[1]-1]] = element
+            
+    return data_dict
+
+#helper function to save the updated resx files back to original location
+def save_update(language):
+    '''
+    language: user's choice of language
+    '''
+    for file in os.listdir(f'{script_path}/Spira-{language}'):
+        #matching name using regex
+        if re.match(r"\w+\." + language + "\.resx", file):
+                
+            #Loops through all key-value pairs for default file paths
+            for prefix, directory in ds.directories.items():
+                #checking if prefix matches the file name
+                if prefix in file:
+                    #renaming the file (getting rid of prefix)
+                    new_name = file.replace(f'{prefix}_','')
+                    os.rename(f'{script_path}/Spira-{language}/{file}',f'{script_path}/Spira-{language}/{new_name}')
+                    #overwriting the original file
+                    if os.path.isfile(f'{dir_path}/{directory}/{new_name}'):
+                        os.remove(f'{dir_path}/{directory}/{new_name}')
+                    shutil.move(f'{script_path}/Spira-{language}/{new_name}',f'{dir_path}/{directory}')
+
+    #deleting temp folder
+    shutil.rmtree(f'{script_path}/Spira-{language}')
 
 
 if __name__ == '__main__':
@@ -155,18 +226,54 @@ if __name__ == '__main__':
             for language in ds.language:
                 import_language(language)
                 
-        #updates current languages
+        #updates missing data to language files
         elif sys.argv[1].lower() == '--update':
-            with open('Buttons1.es.resx','r') as f:
-                data = f.read()
-            Bs_data = BeautifulSoup(data, "xml")
-            Bs_data = list(Bs_data.findAll('data'))
-            my_data = []
-            for element in Bs_data:
-                my_data.append(str(element))
-            print(my_data[144])
-            print(re.search(r'name="\w+"',my_data[144]).string)
-            print(my_data[144][6:25])
+            
+            for language in ds.languages_update:
+                
+                bundle_language(language) #getting language files 
+                
+                for file_name in os.listdir(f'{script_path}/Spira-{language}'):
+                    #matching all files that are primary (english) files
+                    if re.match(r"^[^.]*.resx[^.]*$", file_name):
+                        
+                        #parsing through file to write to it later
+                        eng_tree = ET.parse(f'{script_path}/Spira-{language}/{file_name}')
+                        eng_root = eng_tree.getroot()
+                        lang_file_name = file_name.replace('.',f'.{language}.')
+                        lang_tree = ET.parse(f'{script_path}/Spira-{language}/{lang_file_name}')
+                        lang_root = lang_tree.getroot()
+                        
+                        #making key value pairs of primary and language files
+                        eng_dict = make_dictionary(language,file_name)
+                        lang_dict = make_dictionary(language,lang_file_name)
+
+                        #checking if the data name exists in language file
+                        for key,value in eng_dict.items():
+                            try:
+                                lang_dict[key]
+                            except:
+                                lang_dict[key] = value
+                                
+                                #copying data from primary file to language file
+                                add_string = str(eng_dict[key])
+                                temp_value = re.search(r'<value>[\s\S]*</value>',add_string).span()
+                                temp_string = add_string[temp_value[0]:temp_value[1]]
+                                add_string = add_string.replace(temp_string,f'<value>QQQ-{temp_string}</value>') #adding QQQ to mark missing
+                                add_string = add_string+'\n'
+                                
+                                #adding to language resx file
+                                add_string = ET.fromstring(add_string)
+                                lang_root.append(add_string)
+                        #writing to resx file
+                        lang_tree.write(f'{script_path}/Spira-{language}/{lang_file_name}')
+                
+                #moving updated files back to original location (language only)    
+                save_update(language)
+
+                #logging finished process
+                print(f'{language} Updated')
+                    
                 
         #prints this is argument is invalid      
         else:
